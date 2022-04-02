1363515c242f596ce9ab1ddcc844b3461579e9ba
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-02 08:32:29 +0100

    Lead-time starts today if you order before 12

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -2,10 +2,24 @@ import { PlainDate, PlainDateTime } from "temporal-polyfill";
 
 import { orderCake } from ".";
 
-const Monday = PlainDateTime.from("2022-04-04");
+const morning = (d: PlainDate) => d.toPlainDateTime().with({ hour: 9 });
+const afternoon = (d: PlainDate) => d.toPlainDateTime().with({ hour: 14 });
+
+const Monday = PlainDate.from("2022-04-04");
 const Wednesday = Monday.add({ days: 2 });
+const Thursday = Monday.add({ days: 3 });
 
 test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const result = orderCake("small", Monday);
+  const result = orderCake("small", afternoon(Monday));
+  expect(result).toBeDeliveredOn(Wednesday);
+});
+
+test("A big cake, ordered on Monday, is delivered on Thursday", () => {
+  const result = orderCake("big", afternoon(Monday));
+  expect(result).toBeDeliveredOn(Thursday);
+});
+
+test("A cake order received in the morning starts baking the same day", () => {
+  const result = orderCake("big", morning(Monday));
   expect(result).toBeDeliveredOn(Wednesday);
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -3,5 +3,8 @@ import { PlainDateTime } from "temporal-polyfill";
 type Size = "small" | "big";
 
 export function orderCake(size: Size, orderTime: PlainDateTime) {
-  return orderTime.add({ days: 2 }).toPlainDate();
+  let leadTime = 2;
+  if (size === "big") leadTime++;
+  if (orderTime.hour < 12) leadTime--;
+  return orderTime.add({ days: leadTime }).toPlainDate();
 }
