1d49d146a158aff801a6c486723e10790e389bf1
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-02 07:33:05 +0100

    Remove temporal, fix test

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,14 +1,5 @@
-import * as Temporal from "temporal-polyfill";
-
 import { order } from ".";
 
-const Monday = Temporal.PlainDateTime.from("2022-04-04T14:00:00");
-const Wednesday = Monday.add({ days: 2 });
-
-describe("When a small cake is ordered on Monday afternoon", () => {
-  const deliveryDate = order("small", Monday);
-
-  it("should be delivered Wednesday", () => {
-    expect(deliveryDate).toEqual(Wednesday);
-  });
+test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
+  const result = order("small");
 });
