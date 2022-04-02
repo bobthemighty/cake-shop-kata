23505f00bed4012ea7802c177b76f62befabb6b6
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-02 09:55:25 +0100

    Ensure return from orderCake is a PlainDate

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,13 +1,13 @@
-import { PlainDateTime } from "temporal-polyfill";
+import { PlainDate, PlainDateTime } from "temporal-polyfill";
 
 type Size = "small" | "big";
 
 const isMorning = (d: PlainDateTime) => d.hour < 12;
 
-export function orderCake(size: Size, orderTime: PlainDateTime) {
+export function orderCake(size: Size, orderTime: PlainDateTime): PlainDate {
   const leadTime = size === "small" ? 1 : 2;
   const startDay = isMorning(orderTime)
     ? orderTime
     : orderTime.add({ days: 1 });
-  return startDay.add({ days: leadTime });
+  return startDay.add({ days: leadTime }).toPlainDate();
 }
