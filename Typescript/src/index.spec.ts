e962c96a2dadc133340c0076663faeebdef0b295
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-04-02 09:59:14 +0100

    Introduced CakeRequirements

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -10,16 +10,16 @@ const Wednesday = Monday.add({ days: 2 });
 const Thursday = Monday.add({ days: 3 });
 
 test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const result = orderCake("small", afternoon(Monday));
+  const result = orderCake({ size: "small" }, afternoon(Monday));
   expect(result).toBeDeliveredOn(Wednesday);
 });
 
 test("A big cake, ordered on Monday, is delivered on Thursday", () => {
-  const result = orderCake("big", afternoon(Monday));
+  const result = orderCake({ size: "big" }, afternoon(Monday));
   expect(result).toBeDeliveredOn(Thursday);
 });
 
 test("A cake order received in the morning starts baking the same day", () => {
-  const result = orderCake("big", morning(Monday));
+  const result = orderCake({ size: "big" }, morning(Monday));
   expect(result).toBeDeliveredOn(Wednesday);
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -2,10 +2,17 @@ import { PlainDate, PlainDateTime } from "temporal-polyfill";
 
 type Size = "small" | "big";
 
+export interface CakeRequirements {
+  size: Size;
+}
+
 const isMorning = (d: PlainDateTime) => d.hour < 12;
 
-export function orderCake(size: Size, orderTime: PlainDateTime): PlainDate {
-  const leadTime = size === "small" ? 1 : 2;
+export function orderCake(
+  order: CakeRequirements,
+  orderTime: PlainDateTime
+): PlainDate {
+  const leadTime = order.size === "small" ? 1 : 2;
   const startDay = isMorning(orderTime)
     ? orderTime
     : orderTime.add({ days: 1 });
